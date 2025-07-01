ff39a8ef30c44bf856227a6f1d5799f8273e4bad
Amit Raj <rajamitnine@gmail.com>
2025-07-01 20:45:38 +0530

    Add the CoreConcepts.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
+import { EXAMPLES } from "./data.js";
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
 import { useState } from "react";
+import CoreConcepts from "./components/CoreConcepts.jsx";
 
 function App() {
   const [selectedTopic, setSelectedTopic] = useState();
@@ -29,14 +29,7 @@ function App() {
     <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title} {...conceptItem} />
-            ))}
-          </ul>
-        </section>
+        <CoreConcepts />
         <section id="examples">
           <h2>Examples</h2>
           <menu>
